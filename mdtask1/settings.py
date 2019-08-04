06827649050dccc31a7ab2cc0f9589ecf67e8d39
ltmyth <latimmark@gmail.com>
2019-08-05 00:32:42 +0300

    m t b

--- mdtask1/settings.py
+++ mdtask1/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = [
-#STATIC_DIR 
-#]
+STATICFILES_DIRS = [
+STATIC_DIR 
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 
